d258942650adc72eb885efe2019a95a2742aa108
Rayani Gomes <68917577+RayaniGomes@users.noreply.github.com>
2025-01-11 20:23:54 -0300

    fix: configurações globais

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import StyledComponentsRegistry from "./registry";
+
+export const metadata: Metadata = {
+  title: "Protfolio dos Membros",
+  description: "Site do Protfolio dos Membros da EJECT",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
+      <body>
+        <StyledComponentsRegistry>
+          {children}
+        </StyledComponentsRegistry>
+      </body>
+    </html>
+  );
+}
