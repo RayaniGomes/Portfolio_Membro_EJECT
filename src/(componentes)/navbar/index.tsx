e4ba8c23ece845d8bd00b14727046aa80e850b29
Rayani Gomes <68917577+RayaniGomes@users.noreply.github.com>
2025-01-12 15:11:49 -0300

    fix: link na logo da navbar

--- src/(componentes)/navbar/index.tsx
+++ src/(componentes)/navbar/index.tsx
@@ -1,16 +1,20 @@
 'use client';
 import Image from "next/image";
 import { Nav } from "./styled";
+import Link from "next/link";
 
 export default function Navbar() {
     return (
         <Nav>
-            <Image
-                src="/logo.png"
-                alt="logo"
-                width={158}
-                height={66}
-            />
+            <Link href="/">
+                <Image
+                    src="/logo.png"
+                    alt="logo"
+                    width={158}
+                    height={66}
+                    priority={true}
+                />
+            </Link>
 
             <button>ENTRE EM CONTATO</button>
         </Nav>
