9ba8e6b3971ca37aa96e0d3805ef8b9454bde282
Rayani Gomes <68917577+RayaniGomes@users.noreply.github.com>
2025-01-11 23:01:56 -0300

    fix: responsividade do banner

--- src/(componentes)/benner/styled.ts
+++ src/(componentes)/benner/styled.ts
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 export const Header = styled.header`
     width: 100%;
-    height: 550px;
+    height: 500px;
     display: flex;
     align-items: center;
     justify-content: center;
     margin-top: 100px;
-    background: url('/bg_home.svg') no-repeat center center, var(--gradiente-radial-azul);
+    background: url('/bg_home.svg') no-repeat center center / cover, var(--gradiente-radial-azul);
     text-align: center;
     line-height: 35px;
 
@@ -15,4 +15,31 @@ export const Header = styled.header`
         font-family: "Gwendolyn", serif;
         color: var(--laranja);
     }
-`
+    
+    @media (max-width: 1024px) {
+        height: 400px;
+        line-height: 30px;
+
+        h1,span {
+            font-size: 100px;
+        }
+    }
+
+    @media (max-width: 768px) {
+        height: 300px;
+        line-height: 20px;
+
+        h1, span {
+            font-size: 68px;
+        }
+    }
+
+    @media (max-width: 480px) {
+        height: 250px;
+        line-height: 15px;
+
+        h1, span {
+            font-size: 48px;
+        }
+    }
+`;
